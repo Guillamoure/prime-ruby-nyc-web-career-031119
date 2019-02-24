51697fdfdf8717180fd107b6faf877cc54be9728
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-24 19:30:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
 # Add  code here!
 def prime?(n)
   one_less = n- 1
-  range = Array (1..one_less)
+  range = Array (2..one_less)
   range.each do |x|
-    
+    if n % x == 0
+      return TRUE
+    end
+  else 
+    return FALSE
+  end
   end
 end
